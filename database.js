baecc338aca91f958a535b905721fbe3cd4dc277
logang <gchlawrence31@gmail.com>
2021-03-16 23:09:59 -0400

    final Touches

--- database.js
+++ database.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://172.23.0.2:27017/nodes-db-app',{
+mongoose
+  .connect("mongodb://172.20.0.2:27017/nodes-db-app", {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useFindAndModify: false
-}).then(db => console.log('db is connected'))
-.catch(err=> console.error(err))
+    useFindAndModify: false,
+  })
+  .then((db) => console.log("db is connected"))
+  .catch((err) => console.error(err));
